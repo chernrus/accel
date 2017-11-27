8c74a32be74556eaa20968b937921bce2d151957
cra <cra@em70.ru>
2017-11-27 14:15:53 +0700

    d3 react boston maraphon

--- accel-app/src/index.js
+++ accel-app/src/index.js
@@ -1,8 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as d3 from 'd3';
+
+d3.csv("https://raw.githubusercontent.com/Swizec/server-side-d3-poc/master/src/data.csv")
+  .row(this.rowParse)
+  .get(data =>
+      ReactDOM.hydrate(<App data={data} />, document.getElementById('root'))
+  );
 registerServiceWorker();
